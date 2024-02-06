e9c56ac8f14f0e379c1b666a2131b26d537f5735
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2024-02-06 10:37:55 -0500

    Update global styles and page layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import data from "/data.json";
+import "../app/globals.css";
 
 export default function Home() {
   const list = [
@@ -43,21 +44,39 @@ export default function Home() {
   };
 
   return (
-    <body>
-      <div className="bg-clr-neutral-very-dark-blue h-dvh grid grid-cols-1 grid-rows-1 justify-items-center items-center">
-        <div className=" grid grid-cols-4 grid-rows-2 gap-5 w-9/12 ">
-          <div className=" col-span-1 row-span-2 flex flex-col text-white bg-clr-neutral-dark-blue   rounded-xl">
-            <div className="flex flex-col bg-clr-bg-blue rounded-xl pl-7 gap-10 justify-around">
-              <Image src="/images/image-jeremy.png" alt="Jeremy" width={100} height={100} />
-              <div>
-                <p className="">Report for</p>
+    <body className="">
+      <div className="min-h-screen w-full bg-VeryDarkBlue text-white flex flex-col items-center justify-center py-10 px-3 ">
+        <div className="grid sm:grid-cols-1 md:grid-cols-4 grid-rows-2 gap-5 w-9/12 ">
+          <div className="col-span-1 row-span-2 flex flex-col text-white bg-clr-neutral-dark-blue rounded-xl ">
+            <div className="flex  bg-clr-bg-blue rounded-xl pl-7 pr-2 gap-4  sm:flex-row items-center md:flex-col">
+              <Image
+                className="self-start mt-5"
+                src="/images/image-jeremy.png"
+                alt="Jeremy"
+                width={100}
+                height={100}
+              />
+              <div className="pb-10">
+                <p className="text-clr-neutral-pale-blue">Report for</p>
                 <h2 className="text-3xl">Jeremy Robson</h2>
               </div>
             </div>
-            <div className="flex flex-col items-start  p-7">
-              <button onClick={() => handleTimeframeChange("daily")}>Daily</button>
-              <button onClick={() => handleTimeframeChange("weekly")}>Weekly</button>
-              <button onClick={() => handleTimeframeChange("monthly")}>Monthly</button>
+            <div className="flex  items-start p-7 text-clr-neutral-pale-blue sm:flex-row place-content-center justify-around md:flex-col">
+              <button
+                className={`hover:text-white ${timeframe === "daily" ? "text-white" : ""}`}
+                onClick={() => handleTimeframeChange("daily")}>
+                Daily
+              </button>
+              <button
+                className={`hover:text-white ${timeframe === "weekly" ? "text-white" : ""}`}
+                onClick={() => handleTimeframeChange("weekly")}>
+                Weekly
+              </button>
+              <button
+                className={`hover:text-white ${timeframe === "monthly" ? "text-white" : ""}`}
+                onClick={() => handleTimeframeChange("monthly")}>
+                Monthly
+              </button>
             </div>
           </div>
 
@@ -67,23 +86,25 @@ export default function Home() {
               (listItem) => listItem.name.toLowerCase() === item.title.toLowerCase()
             );
             const colorVariants = {
-              Work: "flex justify-end  bg-clr-work-light-red basis-1/4",
-              Play: "flex justify-end bg-clr-play-soft-blue basis-1/4",
-              Study: "flex justify-end  bg-clr-study-light-red basis-1/4",
-              Exercise: "flex justify-end bg-clr-exercise-lime-green basis-1/4",
-              Social: "flex justify-end bg-clr-social-violet basis-1/4",
-              "Self Care": "flex justify-end bg-clr-self-care-light-orange basis-1/4",
+              Work: "flex justify-end bg-clr-work-light-red  h-16",
+              Play: "flex justify-end bg-clr-play-soft-blue h-16",
+              Study: "flex justify-end bg-clr-study-light-red h-16",
+              Exercise: "flex justify-end bg-clr-exercise-lime-green h-16 ",
+              Social: "flex justify-end bg-clr-social-violet h-16",
+              "Self Care": "flex justify-end bg-clr-self-care-light-orange h-16",
             };
 
             return (
               <div
                 key={index}
-                className="rounded-lg text-white flex flex-col overflow-hidden  bg-clr-neutral-dark-blue  ">
+                className="min-w-28 rounded-lg flex flex-col overflow-hidden bg-clr-neutral-dark-blue ">
                 <div className={`${colorVariants[item.title]} ...`}>
-                  {listItem && <Image src={listItem.img} alt={item.title} width={50} height={50} />}
+                  {listItem && (
+                    <Image priority src={listItem.img} alt={item.title} width={80} height={50} />
+                  )}
                 </div>
-                <div className="flex flex-col p-5  bg-clr-neutral-dark-blue  rounded-lg basis-3/4">
-                  <div className="flex items-center place-content-between ">
+                <div className="flex flex-col p-5 bg-clr-neutral-dark-blue -mt-5 rounded-xl hover:bg-clr-neutral-desaturated-blue">
+                  <div className="flex items-center place-content-between">
                     <h1 className="">{item.title}</h1>
                     <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
                       <path
